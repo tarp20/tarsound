4ae244d9a7eb03b27c029770a4576eaa28ac9de1
Taras Piont <taras.piont@gmail.com>
2021-09-20 20:57:23 +0200

    custom autherisation

--- config/settings.py
+++ config/settings.py
@@ -121,9 +121,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
-
 GOOLE_CLIENT_ID = '239964446455-skl0rpnmqvnb8smm54kfgfhk6qe53bvk.apps.googleusercontent.com'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':
+    ('src.oauth.services.auth_backend.AuthBackend', ),
+    'DEFAULT_PERMISSION _CLASSES': ['rest_framework.permissions.AllowAny']
+}
--- src/oauth/serializers.py
+++ src/oauth/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 
+from .models import AuthUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AuthUser
+        fields = ('avatar', 'country', 'city', 'bio', 'display_name')
 
 class GoogleAuth(serializers.Serializer):
     '''
